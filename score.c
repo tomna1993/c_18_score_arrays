88996314f2a33db3c5089b47312de7796da172ef
TomasNagy <tomi.nagy@outlook.com>
2023-05-07 18:45:38 +0200

    Declare a global variable for counting user input

--- score.c
+++ score.c
@@ -1,20 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
+//Create a global variable to set how many scores we want to average
+const int score_count = 5;
+
 float average(int userScores[], int length);
 
 int main(void)
 {
-	int scores[3];
+	int scores[score_count];
 
 	//Get three numbers from user
-	for(int i = 0; i < 3; i++)
+	for(int i = 0; i < score_count; i++)
 	{
 		scores[i] = get_int("Score%i: ", i);
 	}
 
 	//Print the average of the three given score
-	printf("Average score: %f\n", average(scores, 3));
+	printf("Average score: %f\n", average(scores, score_count));
 }
 
 float average(int userScores[], int length)
