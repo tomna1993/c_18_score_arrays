f15f80ede9e5170b19f86b38e68202dc00298847
TomasNagy <tomi.nagy@outlook.com>
2023-05-07 17:38:34 +0200

    Include cs50 library and ask user for scores

--- score.c
+++ score.c
@@ -1,11 +1,12 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
 	int scores[3];
-	scores[0] = 72;
-	scores[1] = 73;
-	scores[2] = 33;
+	scores[0] = get_int("Score1: ");
+	scores[1] = get_int("Score2: ");
+	scores[2] = get_int("Score3: ");
 
 	//Print the average of the three given score
 	printf("Average score: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
