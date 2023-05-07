2d0b9603ae084410fd6ccc69ecce1b38c6cc7f62
TomasNagy <tomi.nagy@outlook.com>
2023-05-07 17:36:06 +0200

    Create scores array

--- score.c
+++ score.c
@@ -2,10 +2,11 @@
 
 int main(void)
 {
-	int score1 = 72;
-	int score2 = 73;
-	int score3 = 33;
+	int scores[3];
+	scores[0] = 72;
+	scores[1] = 73;
+	scores[2] = 33;
 
 	//Print the average of the three given score
-	printf("Average score: %f\n", (score1 + score2 + score3) / 3.0);
+	printf("Average score: %f\n", (scores[0] + scores[1] + scores[2]) / 3.0);
 }
