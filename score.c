8eb6a0349bb058ccfd401d5b46c01c8338846b91
TomasNagy <tomi.nagy@outlook.com>
2023-05-07 17:30:23 +0200

    Calculate the score and print it out

--- score.c
+++ score.c
@@ -2,5 +2,10 @@
 
 int main(void)
 {
-	
+	int score1 = 72;
+	int score2 = 73;
+	int score3 = 33;
+
+	//Print the average of the three given score
+	printf("Average score: %i\n", (score1 + score2 + score3) / 3);
 }
